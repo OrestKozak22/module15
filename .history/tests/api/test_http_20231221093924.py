8ba9875fa14fc9c4b8fe111c41ad9873419f2ace
Orest Kozak <orestkozak4@gmail.comgit config --global user.email orestkozak4@gmail.comgit config --global user.email orestkozak4@gmail.com>
2023-12-21 09:45:15 +0000

    Додано файли та код для HTTP-тестів

---
+++ .history/tests/api/test_http_20231221093924.py
@@ -0,0 +1,15 @@
+import pytest
+import requests
+
+
+@pytest.mark.http
+def test_first_request():
+    r = requests.get('https://api.github.com/zen')
+    print(f"Response is {r.text}")
+
+@pytest.mark.http
+def test_second_request():
+    r = requests.get('https://api.github.com/users/defunkt')
+    print(f"Response Body is {r.json()}")
+    assert r.status_code == 200
+    print(f"Response Headers are {r.headers}")
